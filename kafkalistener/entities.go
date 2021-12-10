76afca3a23ebea291617d5575e7994600c4bc054
Juan Ramos <disturb16@users.noreply.github.com>
2021-12-10 15:33:24 -0600

    minor fixes (#3)
    - Retry policy configurable
    - Simplified long methods
    - check if message broker is enabled

---
+++ kafkalistener/entities.go
@@ -0,0 +1,44 @@
+package kafkalistener
+
+import (
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-kafka/v2/pkg/kafka"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/hamba/avro"
+	"github.com/hamba/avro/registry"
+	"github.com/sanservices/kit/tls"
+)
+
+type KafkaConfig struct {
+	Enabled         bool     `yaml:"enabled"`
+	Version         string   `yaml:"version"`
+	ConsumeOnly     bool     `yaml:"consume_only"`
+	ConsumerGroupID string   `yaml:"consumer_group_id"`
+	FromOldest      bool     `yaml:"from_oldest"`
+	SchemaReg       string   `yaml:"schema_registration"`
+	Brokers         []string `yaml:"brokers"`
+	TLS             tls.TLS  `yaml:"TLS"`
+}
+
+type MessageBroker struct {
+	enabled          bool
+	publisher        *kafka.Publisher
+	subscriberConfig kafka.SubscriberConfig
+	registryClient   *registry.Client
+	logger           watermill.LoggerAdapter
+	router           *message.Router
+}
+
+type Topic struct {
+	// Name is the name of the topic.
+	Name string
+	// Version is the version of the topica in the schema registry.
+	Version int
+	// Schema is the avro definition of the topic.
+	RawSchema string
+	// Schema is avro schema from the schema registry.
+	Schema avro.Schema
+	// RegisterSchema indicates if the rawSchema should be registered
+	// in the kafka's schema registry.
+	RegisterSchema bool
+}
